2a44bbcfe5e9ebcbf8c5bb8c40cf2705759445dd
eminozkaradeniz1 <emin.ozkaradeniz@ogr.deu.edu.tr>
2019-04-28 12:59:49 +0300

    3_iki yeni class eklendi
    Signed-off-by: eminozkaradeniz1 <emin.ozkaradeniz@ogr.deu.edu.tr>

---
+++ src/OgrenciBilgiSistemiUygulamasi.java
@@ -0,0 +1,7 @@
+public class OgrenciBilgiSistemiUygulamasi {
+
+    public void play(){
+       Lisans lisans = new Lisans("emin","ozkaradeniz",2015045);
+
+    }
+}
---
+++ src/OgretimElemani.java
@@ -0,0 +1,7 @@
+public class OgretimElemani {
+    private String adi;
+
+    public OgretimElemani(String adi){
+        this.adi = adi;
+    }
+}
